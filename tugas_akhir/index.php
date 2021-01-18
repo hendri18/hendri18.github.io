bea15595d738e1ebf9a7906349ff54765145595d
Hendri Heryanto <hendri@senja.co.uk>
2021-01-18 17:13:29 +0700

    tugas akhir

---
+++ tugas_akhir/index.php
@@ -0,0 +1,7 @@
+<?php include "header.php" ?>
+
+<div class="container">
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta, necessitatibus. Eos necessitatibus quaerat omnis placeat alias! Iusto porro facere amet possimus in rerum, est perspiciatis suscipit. Dolorem cupiditate quis magnam?
+</div>
+
+<?php include "footer.php" ?>
